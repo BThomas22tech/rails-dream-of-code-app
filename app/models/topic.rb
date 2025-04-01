47bb68c87288451704d0489c6da2a610e4911c6c
BThomas <bran_thom@yahoo.com>
2025-04-01 13:10:06 -0500

    review changes made

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,4 @@
 class Topic < ApplicationRecord
-    belongs_to :lesson
-    belongs_to :course
+    has_many:lesson
+    
 end
--- db/migrate/20250330200511_create_topics.rb
+++ db/migrate/20250330200511_create_topics.rb
@@ -1,9 +1,7 @@
 class CreateTopics < ActiveRecord::Migration[8.0]
   def change
     create_table :topics do |t|
-      t.string :topic_name
-      t.references :lesson
-      t.references :course
+      t.string :name
       t.timestamps
     end
   end
