91370a77526962bb66a037078630c298e72c58e5
BThomas <bran_thom@yahoo.com>
2025-04-28 15:28:04 -0500

    map function added to loops

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,17 +5,13 @@ class Course < ApplicationRecord
 
   delegate :title, to: :coding_class
   def student_name_list
-    names_list = []
-    self.enrollments.each do |enrollment|
-      names_list << "#{enrollment.student.first_name} #{enrollment.student.last_name}"
+    self.enrollments.map do |enrollment|
+      "#{enrollment.student.first_name} #{enrollment.student.last_name}"
     end
-    names_list
   end
+  
   def student_email_list
-    emails_list = []
-    self.enrollments.each do |enrollment|
-      emails_list << "#{enrollment.student.email}"
+    self.enrollments.map do |enrollment|
+      enrollment.student.email
     end
-    emails_list
   end
-end
